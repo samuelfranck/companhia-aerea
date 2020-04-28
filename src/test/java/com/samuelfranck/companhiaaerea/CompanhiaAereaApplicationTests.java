0424a92b2bd0bcaae0e61f64c14f5b6246526e95
Samuel Franck <samuelfranck.adm@gmail.com>
2020-04-28 19:59:39 -0300

    Projeto compahia-aerea criado

---
+++ src/test/java/com/samuelfranck/companhiaaerea/CompanhiaAereaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.samuelfranck.companhiaaerea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanhiaAereaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
